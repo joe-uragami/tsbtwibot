d28aeec31de98f4d384c256609cee0da1a2ec4da
joe_uragami <s7601096@gmail.com>
2017-05-03 15:21:00 +0900

    ざっと紹介文ランダムツイート、自動RTを実装

--- src/botmain.py
+++ src/botmain.py
@@ -4,7 +4,9 @@ import os
 import sys
 import twitter
 
-
+import json
+import random
+from datetime import datetime, time
 
 # API設定は環境変数で指定
 api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
@@ -14,23 +16,29 @@ api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
                   )
 
 
-def tweet_random():
+def tweet_introduction():
     '''
-    データを読み込み１件ランダムでツイートする
+    部活紹介用のデータを読み込み１件ランダムでツイートする
     :return:
     '''
-    # データ取得処理
+    # データ取得
+    f =  open('introduction.json', 'r')
+    intro_dict = json.load(f)
+
+    intro = random.choice(intro_dict)
 
     # ツイート実行
-    api.PostUpdates("ツイートするやつ")
+    # 複数の画像ツイートがこれでいいのかは検証が必要
+    api.PostUpdates(status = intro['text'], media = intro['img'])
 
 
 def follow_back():
     '''
     フォローした人をフォローする
+    他botで実装済みのためこちらでは実装しない
     :return:
     '''
-
+    pass
 
 
 def tag_retweet():
@@ -38,8 +46,22 @@ def tag_retweet():
     ハッシュタグを検索してリツイートする
     :return:
     '''
+    # データ取得
+    f =  open('hashtags.json', 'r')
+    tags = json.load(f)
 
+    for tag in tags:
+        # タグを検索
+        nowtime = int(time.mktime(datetime.now().timetuple()))
+        results = api.GetSearch(tarm = tag, count=50, result_type='recent')
 
+        for result in results:
+            # 12時間以内の投稿だった場合リツイート
+            if result.created_at_in_seconds in range(nowtime-60*60*12, nowtime):
+                try:
+                    api.PostRetweet(result.id)
+                except:
+                    print('既にリツイートされています')
 
 
 def main(args):
@@ -49,14 +71,17 @@ def main(args):
     :return:
     '''
 
-    # ランダムツイート
-    tweet_random()
+    if "ti" in args:
+        # ランダムツイート
+        tweet_introduction()
 
-    # フォローしてくれた人をフォロー
-    follow_back()
+    if "fb" in args:
+        # フォローしてくれた人をフォロー
+        follow_back()
 
-    # ハッシュタグ検索してリツイート
-    tag_retweet()
+    if "tr" in args:
+        # ハッシュタグ検索してリツイート
+        tag_retweet()
 
 
 if __name__ == '__main__':
