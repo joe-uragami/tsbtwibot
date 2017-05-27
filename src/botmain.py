109b41d2a902d2fe2cba68d3e89f1b84e75280d2
joe_uragami <s7601096@gmail.com>
2017-05-27 22:42:10 +0900

    media_categoryを指定してみた

--- src/botmain.py
+++ src/botmain.py
@@ -30,11 +30,7 @@ def tweet_introduction(api):
 
     # ツイート実行
     # 複数の画像ツイートがこれでいいのかは検証が必要
-    api.PostUpdates(status=intro['text'], media=media_list)
-    # if len(media_list) == 1:
-    #     api.PostUpdates(status=intro['text'], media=media_list[0])
-    # else:
-    #     api.PostUpdates(status=intro['text'], media=media_list)
+    api.PostUpdates(status=intro['text'], media=media_list, media_category='tweet_video')
 
 
 def follow_back(api):
